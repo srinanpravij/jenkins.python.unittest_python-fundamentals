31afc8330319499cfd04989c2d659b990e17ada6
Vijayalakshmi Kuppuswamy <srinanpravij@gmail.com>
2020-12-15 11:43:33 -0500

    modified all the source files

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,10 +1,1 @@
-pipeline {
 
-    stages {
-        stage('SCM Checkout') {
-            steps {
-                sh 'https://github.com/srinanpravij/jenkins.python.unittest_python-fundamentals.git'
-            }
-        }
-
-}
--- src/main/string_evaluator.py
+++ src/main/string_evaluator.py
@@ -1,28 +1,29 @@
 # Created by Leon Hunter at 3:57 PM 10/23/2020
 class StringManipulator(object):
     def get_hello_world(self):
-        return None # TODO - Implement solution
+        return "Hello World"
 
     def concatenate(self, value_to_be_added_to, value_to_add):
-        return None # TODO - Implement solution
+        return (str(value_to_be_added_to) + str(value_to_add))
 
     def substring_inclusive(self, string_to_fetch_from, starting_index, ending_index):
-        return None # TODO - Implement solution
+        return slice(string_to_fetch_from, starting_index, ending_index)
 
     def substring_exclusive(self, string_to_fetch_from, starting_index, ending_index):
-        return None # TODO - Implement solution
+        return slice(string_to_fetch_from, starting_index + 1, ending_index - 1)
 
     def compare(self, first_value, second_value):
-        return None # TODO - Implement solution
+        is_equal = eval(first_value) == eval(second_value)
+        return is_equal
 
     def get_middle_character(self, string_to_fetch_from):
-        return None # TODO - Implement solution
+        return string_to_fetch_from[len(string_to_fetch_from) / 2]
 
     def get_first_word(self, string_to_fetch_from):
-        return None # TODO - Implement solution
+        return string_to_fetch_from.split()[0]
 
     def get_second_word(self, string_to_fetch_from):
-        return None # TODO - Implement solution
+        return string_to_fetch_from.split()[1]
 
     def reverse(self, string_to_be_reversed):
-        return None # TODO - Implement solution
+        return string_to_be_reversed[::-1]
