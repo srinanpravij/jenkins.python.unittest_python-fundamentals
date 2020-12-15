8dac2f24f5b4f304c2a71ee23e83a66ff47f8b6c
Vijayalakshmi Kuppuswamy <srinanpravij@gmail.com>
2020-12-15 11:46:53 -0500

    modified all the source files

--- src/main/string_evaluator.py
+++ src/main/string_evaluator.py
@@ -13,8 +13,17 @@ class StringManipulator(object):
         return slice(string_to_fetch_from, starting_index + 1, ending_index - 1)
 
     def compare(self, first_value, second_value):
-        is_equal = eval(first_value) == eval(second_value)
-        return is_equal
+        if first_value == second_value:
+            return True
+        negative_list = ["0", 0, None, False]
+        positive_list = ["1", 1, True]
+        is_first_value_positive = first_value in positive_list
+        is_first_value_negative = first_value in negative_list
+        is_second_value_positive = second_value in positive_list
+        is_second_value_negative = second_value in negative_list
+        are_both_positive = is_first_value_positive and is_second_value_positive
+        are_both_negative = is_first_value_negative and is_second_value_negative
+        return are_both_negative or are_both_positive
 
     def get_middle_character(self, string_to_fetch_from):
         return string_to_fetch_from[len(string_to_fetch_from) / 2]
